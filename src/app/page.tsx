9ca084567190e9e4228b0757c608cfad403b0c62
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-21 07:38:36 +0530

    added category page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Slider from "@/components/Slider";
 import React from "react";
 import ProductList from "@/components/ProductList";
+import CategoryList from "@/components/CategoryList";
 
 
 const HomePage = () => {
@@ -15,6 +16,16 @@ const HomePage = () => {
             {/*FEATURED PRODUCTS*/}
             <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32">
                 <h1 className="text-2xl">Featured Products</h1>
+                <ProductList/>
+            </div>
+            {/*CATEGORIES*/}
+            <div className="mt-24 ">
+                <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 mb-12">Categories</h1>
+                <CategoryList/>
+            </div>
+            {/*NEW PRODUCTS*/}
+            <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32">
+                <h1 className="text-2xl">New Arrivals</h1>
                 <ProductList/>
             </div>
 
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -8,7 +8,7 @@ const ProductList = () => {
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
 
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22%]">
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
                 <div className="relative w-full h-80">
                     <Image src="https://images.pexels.com/photos/2307303/pexels-photo-2307303.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
                            fill sizes={"25vw"}
@@ -29,6 +29,75 @@ const ProductList = () => {
                 <button type="button" className="rounded-2xl ring-1 ring-lama w-max
                     text-xs py-2 px-4 text-lama hover:bg-lama hover:text-white">
                 Add to Cart</button>
+            </Link>
+
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+                <div className="relative w-full h-80">
+                    <Image src="https://images.pexels.com/photos/2307303/pexels-photo-2307303.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
+                           fill sizes={"25vw"}
+                           className="absolute object-cover rounded-md z-10 hover:opacity-0
+                           transition-opacity easy duration-500"
+                    />
+                    <Image src="https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
+                           fill sizes={"25vw"}
+                           className="absolute object-cover rounded-md z-10 hover:opacity-0
+                           transition-opacity easy duration-500"
+                    />
+                </div>
+                <div className="flex justify-between">
+                    <span className="font-semibold">Product Name</span>
+                    <span className="font-medium">$123</span>
+                </div>
+                <div className="text-sm text-gray-500">My Description</div>
+                <button type="button" className="rounded-2xl ring-1 ring-lama w-max
+                    text-xs py-2 px-4 text-lama hover:bg-lama hover:text-white">
+                    Add to Cart</button>
+            </Link>
+
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+                <div className="relative w-full h-80">
+                    <Image src="https://images.pexels.com/photos/2307303/pexels-photo-2307303.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
+                           fill sizes={"25vw"}
+                           className="absolute object-cover rounded-md z-10 hover:opacity-0
+                           transition-opacity easy duration-500"
+                    />
+                    <Image src="https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
+                           fill sizes={"25vw"}
+                           className="absolute object-cover rounded-md z-10 hover:opacity-0
+                           transition-opacity easy duration-500"
+                    />
+                </div>
+                <div className="flex justify-between">
+                    <span className="font-semibold">Product Name</span>
+                    <span className="font-medium">$123</span>
+                </div>
+                <div className="text-sm text-gray-500">My Description</div>
+                <button type="button" className="rounded-2xl ring-1 ring-lama w-max
+                    text-xs py-2 px-4 text-lama hover:bg-lama hover:text-white">
+                    Add to Cart</button>
+            </Link>
+
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+                <div className="relative w-full h-80">
+                    <Image src="https://images.pexels.com/photos/2307303/pexels-photo-2307303.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
+                           fill sizes={"25vw"}
+                           className="absolute object-cover rounded-md z-10 hover:opacity-0
+                           transition-opacity easy duration-500"
+                    />
+                    <Image src="https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1200" alt={"logo"}
+                           fill sizes={"25vw"}
+                           className="absolute object-cover rounded-md z-10 hover:opacity-0
+                           transition-opacity easy duration-500"
+                    />
+                </div>
+                <div className="flex justify-between">
+                    <span className="font-semibold">Product Name</span>
+                    <span className="font-medium">$123</span>
+                </div>
+                <div className="text-sm text-gray-500">My Description</div>
+                <button type="button" className="rounded-2xl ring-1 ring-lama w-max
+                    text-xs py-2 px-4 text-lama hover:bg-lama hover:text-white">
+                    Add to Cart</button>
             </Link>
 
         </div>

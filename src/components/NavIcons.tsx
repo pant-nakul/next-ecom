3159ceb3e795c30a1c405591596d664a1fc698a6
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-21 06:54:08 +0530

    adjusted notification and cart items counter

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -29,14 +29,21 @@ const NavIcons = () => {
             />
             {isProfileOpen && (
                 <div className="absolute p-4 rounded-md top-12 left-0 text-sm
-                 z-40 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+                 z-40 shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white">
                     <Link href="/profile">Profile</Link>
                     <div className="mt-2 cursor-pointer">Logout</div>
                 </div>
             )}
-            <Image src="/notification.png" alt="Notification" height={22} width={22} className="cursor-pointer"/>
             <div className="cursor-pointer relative">
-                <Image src="/cart.png" alt="Cart" height={22} width={22} className=""
+                <Image src="/notification.png" alt="Notification" height={16} width={16} className="cursor-pointer"/>
+                <div className="absolute -top-4 -right-4 w-6 h-6  bg-blue-400
+                    rounded-full text-white text-sm flex items-center justify-center
+                ">20
+                </div>
+            </div>
+
+            <div className="cursor-pointer relative">
+                <Image src="/cart.png" alt="Cart" height={16} width={16} className=""
                        onClick={() => setIsCartOpen((prev) => !prev)}/>
                 <div className="absolute -top-4 -right-4 w-6 h-6  bg-red-400
                     rounded-full text-white text-sm flex items-center justify-center

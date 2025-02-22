45438cac06aefc96796c8f48ce5873a624fb374b
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-22 17:32:37 +0530

    campaign componnet

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from "next/image";
 
 const ListPage = () => {
     return (
-        <div>ListPage</div>
+        <div className="px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 relative">
+            {/*CAMPAIGN*/}
+            <div className="bg-pink-50 p-4 flex justify-between h-64">
+                {/*TEXT*/}
+                <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+                    <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">Grab upto 50% on<br/> selected Products</h1>
+                    <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
+                        Buy Now</button>
+                </div>
+                {/*IMAGE*/}
+                <div className="relative w-1/3">
+                    <Image src="/woman.png" alt="campaign" fill className={"object-contain"} />
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 export default ListPage

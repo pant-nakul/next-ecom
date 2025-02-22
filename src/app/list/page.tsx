9fddcd6c47c837451a24c52d0f372a9b268e0209
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-22 17:38:36 +0530

    Product list added to list page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from "next/image";
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
 
 const ListPage = () => {
     return (
         <div className="px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 relative">
             {/*CAMPAIGN*/}
-            <div className="bg-pink-50 p-4 flex justify-between h-64">
+            <div className="bg-pink-50 px-4 flex justify-between h-64">
                 {/*TEXT*/}
                 <div className="w-2/3 flex flex-col items-center justify-center gap-8">
                     <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">Grab upto 50% on<br/> selected Products</h1>
@@ -16,9 +18,11 @@ const ListPage = () => {
                 <div className="relative w-1/3">
                     <Image src="/woman.png" alt="campaign" fill className={"object-contain"} />
                 </div>
-
             </div>
-
+            {/*FILTER*/}
+            <Filter/>
+            {/*PRODUCTLIST*/}
+            <ProductList/>
         </div>
     )
 }

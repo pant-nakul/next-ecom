ab166135cd2fbd4e281918f40b04f508c406787c
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-22 21:57:18 +0530

    modified product page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ProductImages from "@/components/ProductImages";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import Add from "@/components/Add";
 
 const SinglePage = () => {
     return (
@@ -10,7 +12,20 @@ const SinglePage = () => {
             </div>
             {/*TEXT*/}
             <div className="w-full lg:w-1/2 flex flex-col gap-6">
-                TEXTS
+                <h1 className="text-4xl font-medium">Product Name</h1>
+                <p className="text-gray-500">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.
+
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.
+                </p>
+                <div className="h-[1px] bg-gray-200"/>
+                <div className=" flex items-center gap-4">
+                    <h3 className="text-xl text-gray-500 line-through">$59</h3>
+                    <h2 className="font-medium text-2xl text-black">$49</h2>
+                </div>
+                <div className="h-[1px] bg-gray-200"/>
+                <CustomizeProducts/>
+                <Add/>
             </div>
         </div>
     )

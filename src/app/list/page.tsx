23c78384b7b1b81ad18093d30005f8bbb7d0aec5
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-22 18:07:37 +0530

    Filters

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -7,7 +7,7 @@ const ListPage = () => {
     return (
         <div className="px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32 relative">
             {/*CAMPAIGN*/}
-            <div className="bg-pink-50 px-4 flex justify-between h-64">
+            <div className="bg-pink-50 px-4 hidden sm:flex justify-between h-64">
                 {/*TEXT*/}
                 <div className="w-2/3 flex flex-col items-center justify-center gap-8">
                     <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">Grab upto 50% on<br/> selected Products</h1>
@@ -22,6 +22,7 @@ const ListPage = () => {
             {/*FILTER*/}
             <Filter/>
             {/*PRODUCTLIST*/}
+            <h1 className="mt-12 text-xl font-semibold">{"Shoes for you!"}</h1>
             <ProductList/>
         </div>
     )
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -3,7 +3,51 @@ import React from 'react'
 
 const Filter = () => {
     return (
-        <div>Filter</div>
+        <div className="mt-12 flex justify-between">
+            <div className="flex flex-wrap gap-6">
+                <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ">
+                    <option value="">Type</option>
+                    <option value="physical">Physical</option>
+                    <option value="digital">DIgital</option>
+                </select>
+                <input type="text" name="min" placeholder="min price"
+                       className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+                />
+                <input type="text" name="max" placeholder="max price"
+                       className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+                />
+                <select name="size" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ">
+                    <option value="">Size</option>
+                    <option value="size">Size</option>
+                    <option value="digital">DIgital</option>
+                </select>
+                <select name="color" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ">
+                    <option value="">Color</option>
+                    <option value="red">Red</option>
+                    <option value="blue">Blue</option>
+                    <option value="yellow">Yellow</option>
+                </select>
+                <select name="category" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ">
+                    <option value="">Category</option>
+                    <option value="physical">New Arrival</option>
+                    <option value="digital">Popular</option>
+                </select>
+                <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ">
+                    <option value="">All Filters</option>
+                </select>
+            </div>
+            <div className="">
+                <select name="sort" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-white
+                        ring-1 ring-gray-400">
+                    <option>Sort By</option>
+                    <option value="physical">Price (low to high)</option>
+                    <option value="physical">Price (high to low)</option>
+                    <option value="physical">Newest</option>
+                    <option value="digital">Oldest</option>
+                </select>
+            </div>
+
+        </div>
     )
 }
 export default Filter
